146c21a72a8c9b72637bddb076109a5652fa985d
Romain Francony <r.francony@gmail.com>
2017-09-07 13:19:01 +0200

    Question 2

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -13,7 +13,7 @@ def average_above_zero(input_list):
         if item > 0:
             positive_value_sum += item
             positive_value_count += 1
-        elif item==0:
+        elif item == 0:
             print('This value is null : ' + str(item))
         else:
             print('This value is negative : ' + str(item))
@@ -21,10 +21,38 @@ def average_above_zero(input_list):
 
     return moy
 
+
 """ #testing average above zero function
 my_list = [1, 2, 3, 4, -7]
 
 result = average_above_zero(my_list)
 message = "The average of positive samples of {list_values} is {res}" .format(list_values=my_list, res=result)
+print(message)
+"""
+
+
+def max_value(list):
+    ##
+    # Basic function able to return the max value of a list
+    # @param list : the list to be scanned
+    # @throws a exception (ValueError) on an empty list
+
+    # Throw exception if the list is empty
+    if len(list) == 0:
+        raise ValueError("The list is empty")
+
+    max = list[0]
+    index_of_max = 0
+    for index, item in enumerate(list):
+        if item > max:
+            max = item
+            index_of_max = index
+    return max
+
+
+"""#testing max_value function
+my_list = [1, 2, 3, 4, -7]
+result = max_value(my_list)
+message = "The max value of {list_values} is : {res}".format(list_values=my_list, res=result)
 print(message)
 """
