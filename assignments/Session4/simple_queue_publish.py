ecb2e4a4607ea6deabf3a1cf0193e3876665c37b
Romain Francony <r.francony@gmail.com>
2017-10-16 16:00:23 +0200

    Queue message concurrency

--- assignments/Session4/simple_queue_publish.py
+++ assignments/Session4/simple_queue_publish.py
@@ -4,6 +4,19 @@
 
 import pika
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-concurrency", action='store_true', help='Activate persistence')
+parser.add_argument("-n", "--sendmany", type=int, default=1)
+concurrency = parser.parse_args().concurrency
+nb_message = parser.parse_args().sendmany
+
+properties = None
+if concurrency:
+    properties = pika.BasicProperties(
+        delivery_mode=2
+    )
 
 amqp_url = "amqp://taumfzlk:P1gefgPdpz3UKtJ7aivorzL8tzpMALuX@lark.rmq.cloudamqp.com/taumfzlk"
 url = os.environ.get('CLOUDAMQP_URL', amqp_url)
@@ -16,9 +29,12 @@ channel = connection.channel()
 message = "Romain Francony"
 
 channel.queue_declare(queue='presentation')
-channel.basic_publish(exchange='',
-                      routing_key='presentation',
-                      body=message)
 
-print "[x] Sent {message}".format(message=message)
+for i in xrange(nb_message):
+    channel.basic_publish(exchange='',
+                          routing_key='presentation',
+                          body=message,
+                          properties=properties)
+
+    print "[x] Sent {message}".format(message=message)
 connection.close()
--- assignments/Session4/simple_queue_read.py
+++ assignments/Session4/simple_queue_read.py
@@ -4,6 +4,11 @@
 
 import pika
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-concurrency", action='store_true', help='Activate persistence')
+concurrency = parser.parse_args().concurrency
 
 amqp_url = "amqp://taumfzlk:P1gefgPdpz3UKtJ7aivorzL8tzpMALuX@lark.rmq.cloudamqp.com/taumfzlk"
 url = os.environ.get('CLOUDAMQP_URL', amqp_url)
@@ -19,13 +24,16 @@ counter = 0
 
 def callback(ch, method, properties, body):
     global counter
+    global concurrency
     counter = counter + 1
     print "[x] Received message : {message}\nTotal of {count} message(s)".format(message=body, count=counter)
+    if concurrency:
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 channel.basic_consume(callback,
                       queue='presentation',
-                      no_ack=True)
+                      no_ack=not concurrency)
 
 print "[x] Waiting for messages. To exit press Ctrl+C"
 channel.start_consuming()
