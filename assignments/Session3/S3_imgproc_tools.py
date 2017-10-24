55d37e69552caa5c62889c19a1176b6df0862396
Romain Francony <r.francony@gmail.com>
2017-10-24 11:59:47 +0200

    Add image thresholding

--- assignments/Session3/S3_imgproc_tools.py
+++ assignments/Session3/S3_imgproc_tools.py
@@ -42,3 +42,37 @@ def invert_colors_numpy(input_img):
 def invert_colors_opencv(input_img):
     img_inv = cv2.bitwise_not(input_img)
     return img_inv
+
+
+def threshold_image_manual(input_img):
+    img_inv = np.zeros(input_img.shape, dtype=np.uint8)
+
+    # Gray scale image
+    if len(input_img.shape) == 2:
+        for x_index in xrange(img_inv.shape[0]):
+            for y_index in xrange(img_inv.shape[1]):
+                if input_img[x_index, y_index] > 127:
+                    img_inv[x_index, y_index] = 255
+                else:
+                    img_inv[x_index, y_index] = 0
+
+        return img_inv
+
+    # Color image
+    for x_index in xrange(img_inv.shape[0]):
+        for y_index in xrange(img_inv.shape[1]):
+            for z_index in xrange(img_inv.shape[2]):
+                if input_img[x_index, y_index, z_index] > 127:
+                    img_inv[x_index, y_index, z_index] = 255
+                else:
+                    img_inv[x_index, y_index, z_index] = 0
+
+    return img_inv
+
+
+def threshold_image_numpy(input_img):
+    return (input_img > 127).astype(np.uint8) * 255
+
+
+def threshold_colors_opencv(input_img):
+    return cv2.threshold(input_img, 127, 255, cv2.THRESH_BINARY)[1]
